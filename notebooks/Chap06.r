af854a9df4510936fd708aff1da826ddda89d31d
Wataru NAKAI <watnk0806@gmail.com>
2018-06-26 17:41:52 +0900

    Change kernel to R 3.4.

--- notebooks/Chap06.r
+++ notebooks/Chap06.r
@@ -1,4 +1,9 @@
 
+# # 6章 一元配置分散分析・多重比較
+
+# In[1]:
+
+
 library(dplyr)
 library(tidyr)
 library(ggplot2)
@@ -6,7 +11,21 @@ library(readr)
 library(gridExtra)
 library(Cairo)
 
+# ## 6.1 t検定の繰り返しは誤り
+
+# In[1]:
+
+
 (1 - 0.05)**3
+
+# ## 6.2 一元配置分散分析: 原理
+# 
+# 群内分散と群間分散
+
+# ## 6.3 Rで計算
+
+# In[2]:
+
 
 d.stron <- data_frame(
     stron = c(28.2, 33.2, 36.4, 34.6, 29.1, 31.0, 
@@ -18,7 +37,21 @@ d.stron <- data_frame(
 )
 head(d.stron)
 
+# In[3]:
+
+
 aov(stron ~ lake, data = d.stron) %>% summary()
+
+# 手動計算
+# 
+# * `m`: 群の総数
+# * `n`: 各群の標本サイズ
+# * `SS.b`: 群間平方和
+# * `SS.w`: 群内平方和
+# * `MS.b`, `MS.w`: 平均平方
+
+# In[4]:
+
 
 (function(){
     m <- d.stron$stron %>% mean()
@@ -40,6 +73,9 @@ aov(stron ~ lake, data = d.stron) %>% summary()
     return(list("F_val" = F_val, "p_val" = p_val))
 })()
 
+# In[5]:
+
+
 d.pig <- data_frame(
     f1 = c(60.8, 57.0, 65.0, 58.6, 61.7),
     f2 = c(68.7, 67.7, 74.0, 66.3, 69.8), 
@@ -48,13 +84,40 @@ d.pig <- data_frame(
 )
 d.pig
 
+# In[6]:
+
+
 d.pig %>% gather(feed, Pig) %>% aov(Pig ~ feed, data = .) %>% summary()
+
+# ### Tuket HSD
+
+# In[7]:
+
 
 aov(stron ~ lake, data = d.stron) %>% summary()
 
+# 標準誤差 SE は，
+# 
+# $SE = \sqrt{\frac{残差MS}{n}}$
+
+# In[9]:
+
+
 sqrt(9.8 / 6)
 
+# 統計量 $q$ は，5 と 1とでは，
+# 
+# $|q| = \frac{\bar{X}_5 - \bar{X}_1}{SE}$
+
+# In[8]:
+
+
 aov(stron ~ lake, data = d.stron) %>% TukeyHSD()
+
+# * スチューデント化されたq とは?
+
+# In[84]:
+
 
 d.stron %>% group_by(lake) %>% summarise_each(funs(mean)) %>% 
     {
@@ -69,79 +132,208 @@ d.stron %>% group_by(lake) %>% summarise_each(funs(mean)) %>%
         }
     }
 
+# ## 多重比較
+# 
+# * 検定の多重性
+#     * 有意水準の調整
+# * 事前比較，事後比較
+# 
+# 種類
+# 
+# * Tukey-Kramer
+# * Dunnett
+# * Scheffe
+# * Williams
+
+# ### 6.4.1 Tukey-Kramer
+
+# * 表6.7（微妙に値が違う）
+
+# In[64]:
+
+
 d <- read_csv("../samplecode/Rで学ぶ統計学入門図版作成用/table6-5.csv") 
 head(d)
 
+# In[65]:
+
+
 aov(score ~ factor(group), data=d) %>% summary()
+
+# In[66]:
+
 
 aov(score ~ factor(group), data=d)
 
+# In[67]:
+
+
 aov(score ~ factor(group), data=d) %>% TukeyHSD()
+
+# g1 と g2 が逆
+
+# In[68]:
+
 
 d %>% tibble::rownames_to_column() %>% spread(group, score) %>% select(-1) %>% sapply(function(x){
     na.omit(x)
 })
 
+# In[73]:
+
+
 d %>% mutate(group = case_when(.$group == "g1" ~ "g2", 
                               .$group == "g2" ~ "g1", 
                               TRUE ~ .$group)) -> d
 
+# In[74]:
+
+
 aov(score ~ factor(group), data = d) %>% TukeyHSD()
+
+# In[75]:
+
 
 options(repr.plot.width = 3, repr.plot.height = 3)
 
+# In[84]:
+
+
 d %>% ggplot(aes(x = group, y = score)) + geom_boxplot()
+
+# ### 6.4.2 Dunnett
+
+# 表6.8
+
+# In[85]:
+
 
 d2 <- read_csv("../samplecode/Rで学ぶ統計学入門図版作成用/table6-6.csv")
 head(d2)
 
+# In[86]:
+
+
 str(d2)
+
+# In[91]:
+
 
 d2 %>% tibble::rownames_to_column("id") %>% spread(group, score) %>% select(-id) %>% sapply(function(x){
     na.omit(x)
 })
 
+# In[98]:
+
+
 library(multcomp)
+
+# In[102]:
+
 
 detach(package:multcomp)
 
+# In[103]:
+
+
 detach(package:TH.data)
+
+# In[104]:
+
 
 detach(package:MASS)
 
+# In[105]:
+
+
 select
+
+# In[106]:
+
 
 d2 %>% 
     mutate(group = factor(group)) %>% 
     aov(score ~ group, data=.) %>% 
     multcomp::glht(linfct=multcomp::mcp(group="Dunnett")) %>% summary()
 
+# In[108]:
+
+
 d2 %>% ggplot(aes(x = group, y = score)) + geom_boxplot()
+
+# ### 6.4.3 Bonferroni (Holm)
+
+# 表6.9
+
+# In[109]:
+
 
 d3 <- read_csv("../samplecode/Rで学ぶ統計学入門図版作成用/table6-7.csv")
 head(d3)
+
+# In[110]:
+
 
 d3 %>% tibble::rownames_to_column("id") %>% spread(group, score) %>% select(-id) %>% sapply(function(x){
     na.omit(x)
 })
 
+# In[115]:
+
+
 d3 %>% ggplot(aes(group, score)) + geom_boxplot()
+
+# 表6.7 のデータ
+
+# In[113]:
+
 
 d %>% {pairwise.t.test(.$score, .$group, p.adj = "holm")}
 
+# 表6.9 のデータ
+
+# In[114]:
+
+
 d3 %>% {pairwise.t.test(.$score, .$group, p.adj = "holm")}
+
+# In[116]:
+
 
 ls()
 
+# ## 演習問題
+# ### ex. 6.1
+
+# In[117]:
+
+
 str(d.pig)
+
+# In[123]:
+
 
 d.pig %>% gather(feed, weight) %>% ggplot(aes(feed, weight)) + geom_boxplot()
 
+# In[120]:
+
+
 d.pig %>% gather(feed, weight) %>% aov(weight ~ factor(feed), data=.) %>% TukeyHSD()
+
+# In[121]:
+
 
 d.pig %>% gather(feed, weight) %>% {pairwise.t.test(.$weight, .$feed, p.adj = "holm")}
 
+# ### ex. 6.2
+
+# In[124]:
+
+
 str(d3)
+
+# In[126]:
+
 
 d3 %>% 
     mutate(group = if_else(group == "g1", "C", group)) %>% 
@@ -149,8 +341,14 @@ d3 %>%
     aov(score ~ group, data=.) %>% 
     multcomp::glht(linfct=multcomp::mcp(group="Dunnett")) %>% summary()
 
+# In[128]:
+
+
 d3 %>% 
     mutate(group = if_else(group == "g1", "C", group)) %>% 
     {pairwise.t.test(.$score, .$group, p.adj = "holm")}
+
+# In[69]:
+
 
 devtools::session_info()
--- notebooks/Chap07.r
+++ notebooks/Chap07.r
@@ -1,9 +1,25 @@
 
+# # 7章
+
+# ## 7.2
+
+# In[1]:
+
+
 library(dplyr)
+
+# In[2]:
+
 
 library(tidyr)
 
+# In[3]:
+
+
 library(ggplot2)
+
+# In[4]:
+
 
 d.room <- data_frame(
     kaiteki = c(6, 5, 6, 7, 2, 1, 1, 0, 7, 8, 8, 9, 8, 7, 6, 7), 
@@ -12,57 +28,137 @@ d.room <- data_frame(
 )
 head(d.room)
 
+# In[164]:
+
+
 write_csv(d.room, "../data/table7-1.csv")
+
+# In[5]:
+
 
 str(d.room)
 
+# p.98
+# 
+# 誤
+# 
+# ```r
+# summary(aov(y ~ size + student + size:student))
+# ```
+# 
+# 正
+# 
+# ```r
+# summary(aov(kaiteki ~ size + student + size:student))
+# ```
+# 
+
+# In[8]:
+
+
 d.room %>% aov(kaiteki ~ size + student + size:student, data = .) %>% summary()
 
+# In[9]:
+
+
 options(repr.plot.width = 3, repr.plot.height = 3)
+
+# In[30]:
+
 
 d.room %>% mutate(size=factor(size, levels=c("S", "L"), labels=c("S", "L")), 
                  student=factor(student)) %>% 
     group_by(size, student) %>% summarise(kaiteki = mean(kaiteki)) %>% 
     ggplot(aes(x = size, y = kaiteki, group=student, colour=student)) + geom_line() + geom_point()
 
+# In[31]:
+
+
 d.room %>% mutate(size=factor(size, levels=c("S", "L"), labels=c("S", "L")), 
                  student=factor(student)) %>% 
     group_by(student, size) %>% summarise(kaiteki = mean(kaiteki)) %>% 
     ggplot(aes(x = student, y = kaiteki, group=size, colour=size)) + geom_line() + geom_point()
 
+# In[32]:
+
+
 library(readr)
+
+# In[50]:
+
 
 d.soil <- read_csv("../samplecode/Rで学ぶ統計学入門図版作成用/table7-4.csv", col_names = FALSE)
 str(d.soil)
+
+# In[52]:
+
 
 d.soil %>% select(plant=X1) %>% mutate(soil = c(rep(1, 10), rep(2, 10), rep(1, 10), rep(2, 10)), 
                   ft = c(rep(1, 20), rep(2, 20)))  -> d.soil
 d.soil
 
+# In[163]:
+
+
 write_csv(d.soil, "../data/table7-2.csv")
 
+# In[54]:
+
+
 d.soil %>% aov(plant ~ soil + ft + soil:ft, data = .) %>% summary()
+
+# In[59]:
+
 
 d.soil %>% mutate(soil = factor(soil, levels=c(1, 2), labels = c("natural", "agricultural")), ft = factor(ft, levels = c(1, 2), labels = c("C", "F"))) %>% 
     group_by(soil, ft) %>% summarise(plant=mean(plant)) %>% 
     ggplot(aes(x = soil, y = plant, group=ft, colour=ft)) + geom_line() + geom_point()
 
+# F値の計算
+
+# In[61]:
+
+
 d.soil %>% group_by(soil, ft) %>% summarise(plant=mean(plant))
+
+# In[62]:
+
 
 d.soil %>% select(-ft) %>% group_by(soil) %>% summarise(plant=mean(plant))
 
+# In[63]:
+
+
 d.soil %>% select(-soil) %>% group_by(ft) %>% summarise(plant=mean(plant))
 
+# In[67]:
+
+
 d.soil %>% summarise(plant=mean(plant)) 
+
+# In[97]:
+
 
 d.soil %>% group_by(soil, ft) %>% summarise(x = mean(plant), n = n()) %>% 
     mutate(x = n * (x - 32.21)**2) %>% ungroup() %>% summarise(sum(x))
 
+# In[80]:
+
+
 d.soil %>% group_by(soil) %>% summarise(x = mean(plant), n = n()) %>% mutate(x = n * (x - 32.21)**2) %>% summarise(sum(x))
+
+# In[81]:
+
 
 d.soil %>% group_by(ft) %>% summarise(x = mean(plant), n = n()) %>% mutate(x = n * (x - 32.21)**2) %>% summarise(sum(x))
 
+# In[88]:
+
+
 d.soil %>% group_by(soil, ft) %>% mutate(x = (plant - mean(plant))**2 ) %>% summarise(x = sum(x)) %>% ungroup() %>% summarise(x = sum(x))
+
+# In[110]:
+
 
 calc_f_value <- function(df){
     cat("要因", "SS", "df", "MS", "F", "\n")
@@ -117,25 +213,56 @@ calc_f_value <- function(df){
 }
 calc_f_value(d.soil) 
 
+# ## 7.4 線形混合モデル: 固定要因とランダム変量要因を取り込む
+
+# In[111]:
+
+
 d.pig <- read_csv("../samplecode/Rで学ぶ統計学入門図版作成用/table7-5.csv")
 str(d.pig)
 
+# In[162]:
+
+
 write_csv(d.pig, "../data/table7-5.csv")
+
+# In[112]:
+
 
 head(d.pig)
 
+# In[114]:
+
+
 options(repr.plot.width=6)
+
+# In[116]:
+
 
 summary(d.pig)
 
+# In[117]:
+
+
 d.pig %>% group_by(treat) %>% summarise(mean(wt))
+
+# In[133]:
+
 
 d.pig %>% ggplot(aes(x = number, y = wt, colour=treat)) + geom_point() + 
     geom_hline(yintercept = 111.5, linetype=2, colour=2) + 
     geom_hline(yintercept = 125.06, linetype=2, colour=4) + 
     geom_hline(yintercept = 118.3, linetype=2)
 
+# In[136]:
+
+
 d.pig %>% mutate(block=factor(block), treat=factor(treat)) %>% aov(wt ~ treat + Error(block / treat), data=.) %>% summary()
+
+# ## 演習問題
+
+# In[139]:
+
 
 d.mouse <- data_frame(
     wt = c(55.4, 49.7, 52.1, 49.5, 53.2, 51.4, 54.3, 
@@ -147,18 +274,41 @@ d.mouse <- data_frame(
 )
 head(d.mouse)
 
+# In[161]:
+
+
 write_csv(d.mouse, "../data/table-ex7-2.csv")
+
+# In[141]:
+
 
 options(repr.plot.width=3)
 
+# In[142]:
+
+
 d.mouse %>% ggplot(aes(x = feed, y = wt, group=gender, colour=gender)) + geom_point()
+
+# In[145]:
+
 
 d.mouse %>% mutate(feed=factor(feed), gender=factor(gender)) %>% group_by(feed, gender) %>% summarise(wt=mean(wt)) %>% 
     ggplot(aes(x=feed, y=wt, group=gender, colour=gender)) + geom_line() + geom_point()
 
+# In[146]:
+
+
 d.mouse %>% aov(wt ~ feed + gender + feed:gender, data=.)
 
+# In[147]:
+
+
 d.mouse %>% aov(wt ~ feed + gender + feed:gender, data=.) %>% summary()
+
+# ### 演習問題7.3
+
+# In[149]:
+
 
 d.exam <- data_frame(
     indiv = c(1:36), 
@@ -173,16 +323,34 @@ d.exam <- data_frame(
 )
 head(d.exam)
 
+# In[160]:
+
+
 write_csv(d.exam, "../data/table-ex7-3.csv")
+
+# In[151]:
+
 
 options(repr.plot.width = 6)
 
+# In[155]:
+
+
 d.exam %>% ggplot(aes(x = indiv, y = score, shape=factor(juku), colour=class)) + geom_point()
+
+# In[157]:
+
 
 d.exam %>% mutate(juku=factor(juku), class=factor(class)) %>% 
     aov(score ~ juku + Error(class / juku), data=.)
 
+# In[158]:
+
+
 d.exam %>% mutate(juku=factor(juku), class=factor(class)) %>% 
     aov(score ~ juku + Error(class / juku), data=.) %>% summary()
+
+# In[33]:
+
 
 devtools::session_info()
